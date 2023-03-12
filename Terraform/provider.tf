e91cc9c9f7298db69ec9b7c9babd8afea1bea654
Ahmed-Shoushaa <https://github.com/Ahmed-Shoushaa>
2023-03-12 23:13:50 +0200

    remove aws credentials

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAYLU7LNOBVWFGTM5F"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
