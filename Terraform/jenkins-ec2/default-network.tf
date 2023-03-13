1fb7bc491eb23378a3b868256a479e2a3c827508
Ahmed-Shoushaa <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-03-13 16:51:46 +0200

    enabled SSH in jenkins-ec2

--- Terraform/jenkins-ec2/default-network.tf
+++ Terraform/jenkins-ec2/default-network.tf
@@ -15,5 +15,47 @@ resource "aws_default_subnet" "default_az1" {
 
 resource "aws_network_interface" "ni-default" {
   subnet_id = aws_default_subnet.default_az1.id
+  security_groups = [aws_security_group.allow_web.id]
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web traffic"
+  vpc_id      = aws_default_vpc.default-vpc.id
+
+  ingress {
+      description      = "HTTPS"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  ingress {
+      description      = "HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ingress {
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+
+  tags = {
+    Name = "allow_web"
+  }
 }
 
