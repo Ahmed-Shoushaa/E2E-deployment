1b5b87ea2c51203c13fe081e602b59ef72554ba1
Ahmed-Shoushaa <https://github.com/Ahmed-Shoushaa>
2023-03-19 14:23:52 +0200

    create eks with eks module in terraform

---
+++ Terraform/EKS/vpc.tf
@@ -0,0 +1,28 @@
+data "aws_availability_zones" "azs" {}
+module "myapp-vpc" {
+  source          = "terraform-aws-modules/vpc/aws"
+  version         = "3.19.0"
+  name            = "myapp-vpc"
+  cidr            = "10.0.0.0/16"
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  azs             = data.aws_availability_zones.azs.names
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"                  = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"         = 1
+  }
+}
