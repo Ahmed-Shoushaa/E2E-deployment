b2e9fc62dbc87799d5d99147494f5e67e4fe26b0
Ahmed-Shoushaa <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-03-19 08:36:57 +0200

    Added user data for jenkins-ec2

--- Terraform/jenkins-ec2/ec2.tf
+++ Terraform/jenkins-ec2/ec2.tf
@@ -12,6 +12,7 @@ resource "aws_instance" "jenkins-ec2" {
     device_index         = 0
   }
   provisioner "remote-exec" {
+
     inline = ["echo 'wait untill SSH is ready'"]
     connection {
       type        = "ssh"
@@ -19,13 +20,32 @@ resource "aws_instance" "jenkins-ec2" {
       private_key = file(local.private_key_path)
       host        = aws_instance.jenkins-ec2.public_ip
     }
-
   }
   provisioner "local-exec" {
     command = "ansible-playbook -i ${aws_instance.jenkins-ec2.public_ip}, --private-key ${local.private_key_path} jenkins-ec2/playbook.yaml"
-    #command = "ansible-playbook -i ${aws_instance.jenkins-ec2.public_ip}, --private-key ${local.private_key_path} playbook.yaml"
   }
 
+  user_data     = <<EOF
+                  #!/bin/bash
+
+                  # install git
+                  sudo yum install git -y
+
+                  # install kubectl
+                  sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+                  sudo chmod +x ./kubectl
+                  sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
+                  #install docker 
+                  sudo yum install docker -y
+                  sudo groupadd docker
+                  sudo systemctl start docker
+                  sudo chmod 666 /var/run/docker.sock
+                  sudo usermod -a -G docker ec2-user
+                  sudo usermod -aG docker jenkins
+                  sudo yum install python3-pip -y
+                  sudo pip3 install docker-compose
+                  EOF
   tags = {
     Name = "jenkins-ec2"
   }
