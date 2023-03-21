25c16b87cedccf8b29895f1d25d6ea6a1436f1c2
Ahmed-Shoushaa <https://github.com/Ahmed-Shoushaa>
2023-03-21 17:23:34 +0200

    make 2 ECR

--- Terraform/ECR/ecr.tf
+++ Terraform/ECR/ecr.tf
@@ -1,5 +1,14 @@
 resource "aws_ecr_repository" "ecr" {
-  name                 = "sprints"
+  name                 = "app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "ecr" {
+  name                 = "db"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
