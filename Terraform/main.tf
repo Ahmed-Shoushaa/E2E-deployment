fcda625e311d019815c3ee9f9536355aadb27385
Ahmed-Shoushaa <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-03-22 17:44:50 +0200

    output ECR URLs

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,3 +13,11 @@ module "EKS" {
 output "Jenkins server ip"{
     value = module.jenkins-ec2.jenkins-server-ip
 }
+
+output "ecr-app-repository-url"{
+    value = module.ECR.app-repo-url
+}
+
+output "ecr-db-repository-url"{
+    value = module.ECR.db-repo-url
+}  
